baad2cc6964e0e407bdff2fe62a30683969295d8
Luke <luke@everlytic.com>
2019-06-24 09:59:45 +0200

    First attempt at installing grunt

--- load-worker.js
+++ load-worker.js
@@ -1,1 +1,2 @@
-importScripts('http://localhost:8000/web-sdk/everlytic-push-sw.js');
+// This is an example file. Don't use this file on your website. Use the file given to you when you set up your project.
+importScripts('http://localhost:8000/src/everlytic-push-sw.js');
