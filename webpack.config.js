afde32f5925bf3d1ab48a777cb6c6332cbd33f43
Luke <luke@everlytic.com>
2019-07-25 10:24:04 +0200

    NT - Babel now transpiles down along with minfying

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const version = JSON.stringify(require("./package.json").version).replace(/\"/g, '');
 
 module.exports = [{
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     entry: './src/everlytic-push-sdk.js',
     output: {
         'filename': 'everlytic-push-sdk-' + version + '.min.js',
-    },
-    plugins: [
-        new MinifyPlugin()
-    ]
+    }
 }, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     entry: './src/everlytic-push-sw.js',
     output: {
         'filename': 'everlytic-push-sw-' + version + '.min.js',
     },
-    plugins: [
-        new MinifyPlugin()
-    ]
 }];
